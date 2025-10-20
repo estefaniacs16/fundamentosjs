ba2f25f734c1cb433d8bfcb64d945238efce7345
Valeria <clasesups02@gmail.com>
2025-10-20 15:33:28 -0500

    Commit inicial

---
+++ let-vs-var.js
@@ -0,0 +1,16 @@
+var nombre = 'Wolverine'
+
+console.log (nombre);
+
+//let nombre = 'Profesor X'; // 
+if (true) {
+    var nombre = 'Magneto';
+    console.log (nombre);
+}
+console.log(nombre)
+
+let i;
+for (let i =0; i<=5; i++){
+    console.log(i);
+}
+console.log(i);
